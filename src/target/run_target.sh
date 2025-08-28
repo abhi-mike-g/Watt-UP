986292a5f8a85a37203fed10a350c2b701daef56
AMG <abhimg20@gmail.com>
2025-08-28 21:40:04 +0530

    Update run_target.sh

--- src/target/run_target.sh
+++ src/target/run_target.sh
@@ -1,1 +1,5 @@
-
+#!/usr/bin/env bash
+sudo systemd-run --unit=demo.target --scope --property=CPUQuota=80% /usr/bin/python3 -c 'import time
+while True:
+    [x*x for x in range(1000000)]
+    time.sleep(0.5)'
